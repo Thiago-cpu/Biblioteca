a2e8cc54884143160c019e6db408bffdc994e1b2
Thiago-cpu <thifran789456323@gmail.com>
2020-11-11 17:55:59 -0300

    cambio-de-estructura2

--- src/Header/Cerrarsesion.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-session_destroy();
-$url =  $_SERVER["REQUEST_URI"];
-$url_components = parse_url($url); 
-parse_str($url_components['query'], $params); 
-$link = $params['link'];
-header("location: $link");
-?>
