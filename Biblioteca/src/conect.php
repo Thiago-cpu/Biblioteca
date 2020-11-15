a11856392332b63ee10a95d0f5938e8d5e5e1d34
Thiago-cpu <thifran789456323@gmail.com>
2020-11-15 04:35:32 -0300

    connection

--- Biblioteca/src/conect.php
+++ Biblioteca/src/conect.php
@@ -1,7 +1,7 @@
 
 <?php
-$mysqli = new mysqli("localhost", "root", "", "libreria");
-// $mysqli = new mysqli("blveo1ybzx7z80x46i4n-mysql.services.clever-cloud.com", "u1dqc2qrqilkz2mq", "mgRy9Tm6tdXFft9aoh3h", "blveo1ybzx7z80x46i4n");
+// $mysqli = new mysqli("localhost", "root", "", "libreria");
+$mysqli = new mysqli("blveo1ybzx7z80x46i4n-mysql.services.clever-cloud.com", "u1dqc2qrqilkz2mq", "mgRy9Tm6tdXFft9aoh3h", "blveo1ybzx7z80x46i4n");
     if ($mysqli->connect_errno) {
         echo "Fallo al conectar a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
     }
