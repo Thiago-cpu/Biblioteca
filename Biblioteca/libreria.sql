3b58d6e11e9adbe9deacb8ca26b5c4c0ea3cc82c
Thiago-cpu <thifran789456323@gmail.com>
2020-11-16 16:54:24 -0300

    likes sql

--- Biblioteca/libreria.sql
+++ Biblioteca/libreria.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 11-11-2020 a las 04:35:30
+-- Tiempo de generación: 16-11-2020 a las 05:27:59
 -- Versión del servidor: 10.4.14-MariaDB
 -- Versión de PHP: 7.4.9
 
@@ -41,19 +41,21 @@ CREATE TABLE `comentarios` (
 INSERT INTO `comentarios` (`idcom`, `id_user`, `id_lib`, `comentario`) VALUES
 (6, 19, 22, 'Buen libro de mi infancia'),
 (7, 19, 17, 'Para pensar'),
-(8, 19, 18, 'Interesante lo que dice el bigote, me hubiera sido útil leerlo hace 2 años, pero gracias'),
+(8, 19, 18, 'Interesante lo que dice el bigote, me hubiera sido Ãºtil leerlo hace 2 aÃ±os, pero gracias'),
 (9, 20, 23, 'Mi libro favorito\r\n'),
 (10, 20, 22, 'Concuerdo con Strariux'),
 (11, 21, 25, 'No me gusta el libro, siento que no es de mi agrado'),
 (12, 21, 18, 'Strariux toxico'),
-(13, 21, 16, 'Eso de pensar rapido a mi no me sale, me dedico de 5 a 8 horas diarias a procesar un pensamiento lentamente y buscar la mejor opción. Thiago Valdiviezo\r\n'),
+(13, 21, 16, 'Eso de pensar rapido a mi no me sale, me dedico de 5 a 8 horas diarias a procesar un pensamiento lentamente y buscar la mejor opciÃ³n. Thiago Valdiviezo\r\n'),
 (16, 21, 21, 'Buen libro, me gusta, es interesante.'),
 (17, 21, 17, 'Buen libro'),
 (18, 22, 26, 'Buen libro'),
 (19, 22, 22, 'Willy te sigo desde chiquito, no se si leas esto, pero me encantan tus videos y tus libros, saludos'),
 (21, 24, 19, 'Me encanto el libro y toda su saga'),
 (22, 24, 21, 'Interesante'),
-(23, 19, 16, 'Listo');
+(23, 19, 16, 'Listo'),
+(24, 21, 16, 'Buenisimo el libro'),
+(25, 21, 26, 'asd');
 
 -- --------------------------------------------------------
 
@@ -90,7 +92,32 @@ INSERT INTO `libros` (`titulo`, `año`, `idioma`, `edit`, `pags`, `sinopsis`, `s
 ('Wigetta. Un viaje magico', '2015-11-08', 'Español', 'Ediciones Temas de Hoy', 192, 'Una historia ilustrada e interactiva que se inspira en el mundo de los videojuegos.\r\nPara Willy y Vegetta, una invasión zombi que fulmina el sitio en el que vives es solo el principio de una gran aventura. Viajes a las profundidades, intensos duelos al amanecer, misteriosas brujas y temibles gigantes...', 'Si', 'Rústica con solapas', 'Guillermo Diaz y Samuel De Luque', 'Aventura, Fantasia', 'libros/portada_wigetta_vegetta777_201503131941.jpg', 'autores/28028332-288-k503504.jpg', 22),
 ('El principito', '1951-09-05', 'Español', 'Emecé Editores', 111, 'El principito es una narración corta del escritor francés Antoine de Saint-Exupéry, que trata de la historia de un pequeño príncipe que parte de su asteroide a una travesía por el universo, en la cual descubre la extraña forma en que los adultos ven la vida y comprende el valor del amor y la amistad.', 'No', 'Tapa Dura', 'Antoine de Saint-Exupéry', 'Novela, Fantasia', 'libros/9788478887194.jpg', 'autores/17191.jpg', 23),
 ('Harry Potter y la piedra filosofal', '1997-06-30', 'Ingles', 'Bloomsbury Publishing', 223, 'La obra comienza cuando Harry Potter siendo un bebe es dejado en la puerta de la casa de sus tíos por tres magos, quienes comentan que el mago más tenebroso había sido vencido por Harry, quien siendo apenas un bebe sobrevivió a la maldición de un poderoso mago que mato a sus padres. La historia continua cuando el niño cumplió diez años y seguía sin saber que era un mago.', 'Si', 'Tapa Dura', 'J. K. Rowling', 'Novela', 'libros/517DxWbJNZL._SY445_QL70_ML2_.jpg', 'autores/7b7b00b295974cd02f285f3bb1f94ae9ce-3-jk-rowling.rvertical.w1200.jpg', 25),
-('Cien años de soledad', '1967-06-05', 'Español', 'Sudamericana', 471, 'El libro narra la historia de la familia Buendía a lo largo de siete generaciones en el pueblo ficticio de Macondo. ... En medio del camino José Arcadio Buendía tiene un sueño en que se le aparecen construcciones con paredes de espejo y, preguntando su nombre, le responden \"Macondo\".\r\n', 'No', 'Cuero', 'Gabriel García Márquez', 'Novela', 'libros/220px-Cien_años_de_soledad.png', 'autores/gabrielgarciamarquez12.jpg', 26);
+('Cien años de soledad', '1967-06-05', 'Español', 'Sudamericana', 471, 'El libro narra la historia de la familia Buendía a lo largo de siete generaciones en el pueblo ficticio de Macondo. ... En medio del camino José Arcadio Buendía tiene un sueño en que se le aparecen construcciones con paredes de espejo y, preguntando su nombre, le responden \"Macondo\".\r\n', 'No', 'Cuero', 'Gabriel García Márquez', 'Novela', 'libros/220px-Cien_años_de_soledad.png', 'autores/gabrielgarciamarquez12.jpg', 26),
+('Cualquiercosa', '2020-11-05', 'asdwqeq', 'wqewqe', 2, 'wqewqe', 'No', 'wqewqewqe', 'asd', 'asdqwe', 'libros/Captura de pantalla (15).png', 'autores/Captura de pantalla (21).png', 27);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `likes`
+--
+
+CREATE TABLE `likes` (
+  `likeid` int(11) NOT NULL,
+  `idcom` int(11) DEFAULT NULL,
+  `iduser` int(11) DEFAULT NULL,
+  `likes` int(11) DEFAULT 0,
+  `dislikes` int(11) DEFAULT 0,
+  `idlibro` int(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `likes`
+--
+
+INSERT INTO `likes` (`likeid`, `idcom`, `iduser`, `likes`, `dislikes`, `idlibro`) VALUES
+(15, 13, 21, 0, 0, 16),
+(16, 23, 21, 0, 1, 16),
+(17, 24, 21, 1, 0, 16);
 
 -- --------------------------------------------------------
 
@@ -117,7 +144,7 @@ CREATE TABLE `usuarios` (
 INSERT INTO `usuarios` (`nombre`, `apellido`, `fecha_nac`, `nickname`, `email`, `contra`, `id_user`, `user_img`, `admin`) VALUES
 ('Alejo', 'Ironi', '2000-11-14', 'Strariux', 'alejo.simonian9@gmail.com', 'alejoironi', 19, 'usuarios/FuerzaNatural.jpg', b'1'),
 ('Tomas', 'Canosa', '2003-01-18', 'Abrocoly', 'Abrocoly@gmail.com', 'abrocoly123', 20, 'usuarios/Screenshot_4.png', b'0'),
-('Thiago', 'Valdiviezo', '2002-08-14', 'Pasta', 'Pasta@gmail.com', 'Pasta123', 21, 'usuarios/Captura de pantalla 2020-11-08 225917.png', b'0'),
+('Thiago', 'Valdiviezo', '2002-08-14', 'Pasta', 'Pasta@gmail.com', 'Pasta123', 21, 'usuarios/Captura de pantalla 2020-11-08 225917.png', b'1'),
 ('Lucas', 'Torres', '2002-11-28', 'Wachiraton', 'Wachiraton@gmail.com', 'wachiwachi', 22, 'usuarios/9a9801955564ce4d2376fff92bca1615.jpg', b'0'),
 ('Leandro', 'Britez', '2003-03-02', 'Wachirata', 'Wachirata@gmail.com', 'Wachiarata123', 23, 'usuarios/default.PNG', b'0'),
 ('Fiorella', 'Rodriguez', '2002-02-01', 'Fiore', 'fiorerodriguez@gmail.com', 'Fio123', 24, 'usuarios/MP-Stephen_King.png', b'0'),
@@ -143,6 +170,14 @@ ALTER TABLE `libros`
   ADD PRIMARY KEY (`id`);
 
 --
+-- Indices de la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD PRIMARY KEY (`likeid`),
+  ADD KEY `idcom` (`idcom`),
+  ADD KEY `iduser` (`iduser`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
@@ -156,13 +191,19 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `comentarios`
 --
 ALTER TABLE `comentarios`
-  MODIFY `idcom` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `idcom` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT de la tabla `libros`
 --
 ALTER TABLE `libros`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
+
+--
+-- AUTO_INCREMENT de la tabla `likes`
+--
+ALTER TABLE `likes`
+  MODIFY `likeid` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT de la tabla `usuarios`
@@ -180,6 +221,13 @@ ALTER TABLE `usuarios`
 ALTER TABLE `comentarios`
   ADD CONSTRAINT `comentarios_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `usuarios` (`id_user`) ON DELETE CASCADE,
   ADD CONSTRAINT `comentarios_ibfk_2` FOREIGN KEY (`id_lib`) REFERENCES `libros` (`id`) ON DELETE CASCADE;
+
+--
+-- Filtros para la tabla `likes`
+--
+ALTER TABLE `likes`
+  ADD CONSTRAINT `likes_ibfk_1` FOREIGN KEY (`idcom`) REFERENCES `comentarios` (`idcom`),
+  ADD CONSTRAINT `likes_ibfk_2` FOREIGN KEY (`iduser`) REFERENCES `usuarios` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
